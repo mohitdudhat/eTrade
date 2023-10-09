1b413e4e0bf7cc676dfff220f5734f0b0c299067
mohitdudhat <355mohitdudhat@gmail.com>
2023-10-09 16:58:47 +0530

    MERN Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./Components/Navbar";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
